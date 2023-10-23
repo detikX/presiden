4cf437bd133523f616363428ef4e5d2fa2041433
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-10-23 10:50:14 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -6,3 +6,5 @@ $(document).ready(function () {
 $(".new-one .container .camera, .new-one .container .cerutu, .new-one .container .bg").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
 $(".new-one .container .lingkaran").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+$(".new-one .container .data-foto, .new-one .container .data-anak").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
