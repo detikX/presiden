38ba89a47d2d57bdeecb39f132c029e8413fa6de
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-10-24 09:52:10 +0700

    sws

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ $(".new-one .container .camera, .new-one .container .cerutu, .new-one .container
 
 $(".new-one .container .lingkaran").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
+$(".new-one .container .data").attr({ "data-aos": "fade-left", "data-aos-delay": "600", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
+
+
 $(".new-one .container .data-foto, .new-one .container .data-anak").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
 // $("li.myClass").prepend("<i class=\"icon-chevron-right\"></i>");
