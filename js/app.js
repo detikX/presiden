8d797a15b6a456d41dd8cddc9c6f153a7e367535
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-11-21 14:54:03 +0700

    sws

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 
     AOS.init();
+
 })
 
 $(".new-one .container .camera, .new-one .container .motor, .new-one .container .cerutu, .new-one .container .planting, .new-one .container .lukis, .new-one .container .bg").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
-$(".new-one .container .lingkaran").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
-
+$(".new-one .container .lingkaran").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 $(".new-one .container .data").attr({ "data-aos": "fade-left", "data-aos-delay": "600", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
 
 
-$(".new-one .container .data-foto, .new-one .container .data-anak").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
+// $(".new-one .container .data-foto, .new-one .container .data-anak").attr({ "data-aos": "fade-up", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
 // $("li.myClass").prepend("<i class=\"icon-chevron-right\"></i>");
