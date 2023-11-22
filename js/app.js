82e3692827f2a943a2f8a714324c49950a328d62
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-11-22 11:12:13 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
 })
 
-$(".new-one .container .camera, .new-one .container .motor, .new-one .container .cerutu, .new-one .container .planting, .new-one .container .lukis, .new-one .container .bg").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
+$(".new-one .container .camera, .new-one .container .motor, .new-one .container .cerutu, .new-one .container .planting, .new-one .container .lukis, .new-one .container .bg, #jud .headline").attr({ "data-aos": "zoom-in", "data-aos-delay": "300", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
 
 $(".new-one .container .lingkaran").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "300", "data-aos-offset": "10", "data-aos-easing": "linear" })
 $(".new-one .container .data").attr({ "data-aos": "fade-left", "data-aos-delay": "600", "data-aos-duration": "600", "data-aos-offset": "10", "data-aos-easing": "linear" })
